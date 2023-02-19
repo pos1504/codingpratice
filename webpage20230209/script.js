75fb20b85098154db4fa73174c60e3deb74a3d93
pos1504 <pos1504@gmail.com>
2023-02-20 00:40:56 +0900

    use jquery and change html name

--- webpage20230209/script.js
+++ webpage20230209/script.js
@@ -1,30 +1,34 @@
 let night_day = document.getElementById('night_day');
+
 let Body = {
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
     },
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        //document.querySelector('body').style.color = color;
+        $('body').css('color',color);
     }
 }
 
 let Links = {
-    setColor : function(color){
-        let alist = document.querySelectorAll('a');
-        let i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
-
+     setColor : function(color){
+    //     let alist = document.querySelectorAll('a');
+    //     let i = 0;
+    //     while (i < alist.length) {
+    //         alist[i].style.color = color;
+    //         i = i + 1;
+    //     }
+    $('a').css('color',color); // jquery 
     },
 }
 
 
 function night_day_click() {
     if (night_day.value == 'night') {
-        Body.setBackgroundColor('black');
-        Body.setColor('white');
+        
+         Body.setBackgroundColor('black');
+         Body.setColor('white');
         night_day.innerHTML = 'day';
 
         Links.setColor('powderblue');
@@ -32,8 +36,8 @@ function night_day_click() {
         night_day.value = 'day';
     }
     else {
-        Body.setBackgroundColor('white');
-        Body.setColor('black');
+         Body.setBackgroundColor('white');
+         Body.setColor('black');
         night_day.innerHTML = 'night';
 
         Links.setColor('blue');
