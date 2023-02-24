7e0b6588fd7c65c036fda30748967db5856d34f5
pos1504 <pos1504@gmail.com>
2023-02-25 02:47:40 +0900

    React state example 1

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,55 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(props){
+function Header(props) {
   return <header>
-    <h1><a href="/" onClick={(event)=>{
+    <h1><a href="/" onClick={(event) => {
       event.preventDefault();
-      props.onChangeMode(); 
+      props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
 }
 
-function Nav(props){
-  const lis = [  ];
-  for(let i=0 ; i < props.topics.length ; i++){
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key ={t.id} ><a id = {t.id} onClick ={(event)=>{
+    lis.push(<li key={t.id} ><a id={t.id} onClick={(event) => {
       event.preventDefault();
       props.onChangeMode(event.target.id); //t.id
     }}
-      href={"/read/"+t.id} >{t.title}
-      </a></li>);
+      href={"/read/" + t.id} >{t.title}
+    </a></li>);
   }
   return <nav>
     <ol>
-    {lis}   
+      {lis}
     </ol>
   </nav>
 }
-function Article(props){
+function Article(props) {
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
 }
 function App() {
+  const mode = 'READ';
   const topics = [
-    {id:1, title:'html', body:'html is ....'},
-    {id:2, title:'css', body:'css is ....'},
-    {id:3, title:'javascript', body:'javascript is ....'},
+    { id: 1, title: 'html', body: 'html is ....' },
+    { id: 2, title: 'css', body: 'css is ....' },
+    { id: 3, title: 'javascript', body: 'javascript is ....' },
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    content = <Article title="Welcome" body="Hello, READ"></Article>
+  }
   return (
     <div>
-      <Header title = "REACT" onChangeMode={()=>{
+      <Header title="REACT" onChangeMode={() => {
         alert("Header");
       }}></Header>
-      <Nav topics ={topics} onChangeMode={(id)=>{
+      <Nav topics={topics} onChangeMode={(id) => {
         alert(id);
       }}></Nav>
-      <Article title = "Welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
   );
 }
- 
+
 export default App;
+
+
+
